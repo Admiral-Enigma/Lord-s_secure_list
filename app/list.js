0ebb1685ea10ee3ed8596aeaaf86cc6b6999f385
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2017-01-12 13:33:59 +0100

    Started work on the ui

--- app/list.js
+++ app/list.js
@@ -1,1 +1,30 @@
+var mes = []
 
+var mesInput = input()
+
+var el = listMessages(mes, handleClick)
+
+function listMessages(list, onclick){
+  return yo`<div>encrypted Messasges
+    <ul>
+    ${list.map(function(item){
+      return yo`<li>${item}</li>`
+    })}
+    </ul>
+    ${mesInput}<button onclick=${onclick}>Add</button>
+  </div>`
+}
+
+function input(){
+  return yo`<input type="text">`
+}
+
+function handleClick() {
+  mes.push(mesInput.value)
+  mesInput.value = ''
+
+  var newEl = listMessages(mes, handleClick)
+  yo.update(el, newEl)
+}
+
+document.body.appendChild(el)
